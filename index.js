a240efe843039a98f2411818849f430b819e90d9
Obeida Arafa <obeidaarafa@gmail.com>
2025-04-15 20:53:50 +0300

    create book model

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
+app.use(express.json());
 
 const url = process.env.MONGO_URI;
-mongoose.connect(url).then(()=>{
-    console.log("mongoose server started");
-})
+mongoose.connect(url)
+    .then(()=>console.log("MongoDB connected successfully"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
